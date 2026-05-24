5c2eeb9d26a9f17ae3cbcd4a228fa4b4bd9b99b2
Senosqe <luhhdim@gmail.com>
2026-05-25 05:49:30 +0700

    Pembaruan APK Dekstop

---
+++ convert.js
@@ -0,0 +1,9 @@
+const pngToIco = require('png-to-ico');
+const fs = require('fs');
+
+pngToIco('build/icon.png')
+  .then(buf => {
+    fs.writeFileSync('build/icon.ico', buf);
+    console.log('Icon converted to .ico successfully!');
+  })
+  .catch(console.error);
